86bbbefad60618b544b416b4346a1422778e63f9
Matt <168368157+Voobane@users.noreply.github.com>
2024-10-05 21:34:53 -0400

    updated

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const User = require('./User');  // Correct path to User.js
-const Post = require('./Post');  // Correct path to Post.js
+const User = require('./user');  // Correct path to User.js
+const Post = require('./post');  // Correct path to Post.js
 
 // Associations (if any)
 User.hasMany(Post, {
