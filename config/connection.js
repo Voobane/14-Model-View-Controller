1b5e247c09c4ed64acbd1a5a721e6ea7031e2872
Matt <168368157+Voobane@users.noreply.github.com>
2024-10-05 21:13:50 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,15 @@
-let sequelize;
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-    },
-  );
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres',  // Assuming PostgreSQL
+    port: 5432
+  }
+);
 
-// For  localhost use
-
-//const Sequelize = require('sequelize');
-//require('dotenv').config();
-
-//const sequelize = new Sequelize(
-  //process.env.DB_NAME,
-  //process.env.DB_USER,
-  //process.env.DB_PASSWORD,
-  //{
-    //host: 'localhost',
-    //dialect: 'postgres',
-    //port: 5432
-  //}
-//);
-
-//module.exports = sequelize;
+module.exports = sequelize;
