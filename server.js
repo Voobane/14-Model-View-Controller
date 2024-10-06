9e9336d61e42ac354728200f8857c8a94561b6d1
Matt <168368157+Voobane@users.noreply.github.com>
2024-10-05 20:46:37 -0400

    server js code

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
+// Configure session middleware
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -23,17 +25,24 @@ const sess = {
   }),
 };
 
+// Middleware for handling session
 app.use(session(sess));
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set up static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up Handlebars as the view engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Use the routes
 app.use(routes);
 
+// Sync Sequelize models to the database and start the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on port ' + PORT));
 });
