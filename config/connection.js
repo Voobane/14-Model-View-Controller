09ce53631b39c49a92c4c8cf7abef6b2c033a890
Voobane <168368157+Voobane@users.noreply.github.com>
2024-10-11 10:54:10 -0400

    got ride of caps

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@
 //   process.env.DB_USER,
 //   process.env.DB_PASSWORD,
 //   {
-//     host: 'localhost',
+//     host: '127.0.0.1',
 //     dialect: 'postgres',
 //     port: 5432
 //   }
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeController');
-const dashboardRoutes = require('./dashboardController');
+const homeRoutes = require('./homecontroller');
+const dashboardRoutes = require('./dashboardcontroller');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
+ 
