d551374bad2ddc71f21c7c9f2ddfedae2a21fb9c
Matt <168368157+Voobane@users.noreply.github.com>
2024-10-20 20:59:27 -0400

    redo-ing project

--- controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const postRoutes = require('./postroutes');
-const userRoutes = require('./userroutes');
-
-router.use('/posts', postRoutes);
-router.use('/users', userRoutes);
-
-module.exports = router;
