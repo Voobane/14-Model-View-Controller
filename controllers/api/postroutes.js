5a738d241789fdaa9d0eac8c3256dde52c3589bb
Matt <168368157+Voobane@users.noreply.github.com>
2024-10-21 12:36:29 -0400

    rename

--- controllers/api/postroutes.js
+++ controllers/api/postroutes.js
@@ -1,10 +1,9 @@
-const router = require('express').Router();
-const { Post } = require('../../models');
-const withAuth = require('../../utils/authentication');
-
+const router = require("express").Router();
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // POST /api/posts - Create a new post
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
       title: req.body.title,
@@ -18,13 +17,13 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 // DELETE /api/posts/:id - Delete a post
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete("/:id", withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
       where: { id: req.params.id, userId: req.session.user_id },
     });
     if (!postData) {
-      res.status(404).json({ message: 'No post found!' });
+      res.status(404).json({ message: "No post found!" });
       return;
     }
     res.status(200).json(postData);
@@ -34,8 +33,6 @@ router.delete('/:id', withAuth, async (req, res) => {
 });
 
 module.exports = router;
-
-
 
 // login router is missing
 // alos make all folders lowercase
