66babbd0ecc8b688c5f77d95f0d1f227cd062a13
Matt <168368157+Voobane@users.noreply.github.com>
2024-10-20 20:38:43 -0400

    turn to local

--- config/connection.js
+++ config/connection.js
@@ -1,40 +1,40 @@
 //-----------------------Local user only----------------------
 
-// const Sequelize = require("sequelize");
-// require("dotenv").config();
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: "127.0.0.1",
-//     dialect: "postgres",
-//     port: 5432,
-//   }
-// );
-
-// module.exports = sequelize;
-
-//-----------------------Render user only----------------------
-
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-let sequelize;
-
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: "localhost",
-      dialect: "postgres",
-    }
-  );
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "127.0.0.1",
+    dialect: "postgres",
+    port: 5432,
+  }
+);
 
 module.exports = sequelize;
+
+//-----------------------Render user only----------------------
+
+// const Sequelize = require("sequelize");
+// require("dotenv").config();
+
+// let sequelize;
+
+// if (process.env.DB_URL) {
+//   sequelize = new Sequelize(process.env.DB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: "localhost",
+//       dialect: "postgres",
+//     }
+//   );
+// }
+
+// module.exports = sequelize;
