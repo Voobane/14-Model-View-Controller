18c8ec8fd705f86b169b4ebb362557e6a48ff51a
Voobane <168368157+Voobane@users.noreply.github.com>
2024-10-22 12:09:46 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -27,7 +27,7 @@ if (process.env.DB_URL) {
     process.env.DB_USER,
     process.env.DB_PW,
     {
-      host: DB_HOST, //<<< was localhost
+      host: "dpg-cs0u15a3esus7398l8g0-a", //<<< was localhost
       dialect: "postgres",
       port: 5432, //<<< delet if not worked
     }
