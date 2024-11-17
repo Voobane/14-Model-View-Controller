a6aae2733a8c7094314e790b6c995ba37662c153
Matt <168368157+Voobane@users.noreply.github.com>
2024-11-17 11:31:15 -0500

    fixed 404 and fixed footer date

--- controllers/index.js
+++ controllers/index.js
@@ -3,17 +3,27 @@ const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
 const dashboardRoutes = require("./dashboard-routes");
 
-// Use route handlers
+// Use routes
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 router.use("/dashboard", dashboardRoutes);
 
-// Catch-all route for undefined paths
+// Catch 404s
 router.use((req, res) => {
   res.status(404).render("404", {
-    message: "Page not found",
-    logged_in: req.session.logged_in,
     title: "404 Not Found",
+    message: "The page you requested could not be found.",
+    logged_in: req.session.logged_in,
+  });
+});
+
+// Error handler
+router.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render("error", {
+    title: "Error",
+    error: "Something went wrong!",
+    logged_in: req.session.logged_in,
   });
 });
 
